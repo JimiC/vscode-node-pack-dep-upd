418c70c7ca316179f7ccad3d693df9d4f41a562a
JimiC <jimikar@gmail.com>
2018-02-23 11:28:55 +0200

    Add version resolver class.

--- src/versionResolver.ts
+++ src/versionResolver.ts
@@ -1,21 +1,39 @@
-import { WorkspaceConfiguration } from 'vscode';
+import { Uri, WorkspaceConfiguration } from 'vscode';
+import { IDependencies, IPackageDependencies } from './models';
 
 export class VersionResolver {
 
-  private readonly _registry: string;
+  private readonly _registry: Uri;
   private readonly _policy: string;
   private readonly _keepRange: boolean;
 
   constructor(config: WorkspaceConfiguration) {
-    this._registry = config.get<string>('registry');
+    this._registry = Uri.parse(config.get<string>('registry'));
     this._policy = config.get<string>('policy');
     this._keepRange = config.get<boolean>('keepRange');
   }
 
-  public async resolve(dependecies: any): Promise<any> {
+  public async resolve(dependencies: IPackageDependencies): Promise<IPackageDependencies> {
     console.dir(this._registry);
     console.dir(this._policy);
     console.dir(this._keepRange);
-    return Promise.resolve(dependecies);
+
+    const depends = this._getAggregatedDependencies(dependencies);
+
+    for (const dep in depends) {
+      if (!Reflect.has(depends, dep)) { continue; }
+
+      // TODO: resolve version on criteria
+      console.dir(depends[dep]);
+    }
+
+    return Promise.resolve(dependencies);
+  }
+
+  private _getAggregatedDependencies(dependencies: IPackageDependencies): IDependencies {
+    return Reflect.ownKeys(dependencies)
+      .filter(key => dependencies[key])
+      .map<IDependencies>(key => dependencies[key])
+      .reduce((p: IDependencies, c: IDependencies) => ({ ...p, ...c }), {});
   }
 }
